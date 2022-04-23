7ee052419363f4322d2fdaff4bf6c5171b631f04
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-23 15:45:43 +0530

    feat: added configuration

--- src/main/kotlin/io/cherrytechnologies/photoappconfigserver/PhotoAppConfigServerApplication.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappconfigserver/PhotoAppConfigServerApplication.kt
@@ -2,10 +2,12 @@ package io.cherrytechnologies.photoappconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class PhotoAppConfigServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<PhotoAppConfigServerApplication>(*args)
+    runApplication<PhotoAppConfigServerApplication>(*args)
 }
